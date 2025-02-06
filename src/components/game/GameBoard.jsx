c64b64c4085abd3151b0841a6645856993afb486
frarosset <37275581+frarosset@users.noreply.github.com>
2025-02-06 17:31:59 +0100

    feat(GameBoard): update click callback to select cards on click
    The useSelectedCards() custom hook has been used to set a card as
    selected and to check that condition.
    
    Then, the click callback on the .gameboard element has been modified to
    select cards on click.
    
    A single event listener is used exploiting event delegation. When a
    click is captured, first is is checked whether there is a card in the
    clicked point.
    
    In that case, if the associated card id has already been selected, an
    alert is shown. Otherwise, the card is set as selected and the game
    temporarily proceeds as before (incrementing the deck size).
    
    For development only, a style has been applied to the selected cards, to
    highlight them.

--- src/components/game/GameBoard.jsx
+++ src/components/game/GameBoard.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Card from "./Card.jsx";
 import useDeck from "./useDeck.js";
+import useSelectedCards from "./useSelectedCards.js";
 import "../../styles/GameBoard.css";
 
 function GameBoard() {
@@ -15,8 +16,32 @@ function GameBoard() {
   // triggered when the operation is completed
   const deck = useDeck(deckSize);
 
+  // The selected cards are handled internally by the useDeck custom hook.
+  // Two functions are exposed, both of which require the card id:
+  // one can be used to check whether a card has already been selected, and
+  // the other to acknowledge that the associated card has been selected.
+  const { isSelectedCard, setSelectedCard } = useSelectedCards();
+
   // Temporary click callback to test the deck handling of useDeck
-  const clickCallback = () => {
+  // Event delegation is used: when a click is captured, we have to check
+  // whether there is a card in the clicked point.
+  const clickCallback = (e) => {
+    const card = e.target;
+    if (![...card.classList].includes("card")) {
+      return;
+    }
+
+    const cardId = card.getAttribute("data-id");
+
+    if (isSelectedCard(cardId)) {
+      // code for clicking on a selected card (todo)
+      alert("Game over!");
+      return;
+    }
+
+    // code for clicking on an unselected card (todo)
+    setSelectedCard(cardId);
+
     setDeckSize((x) => x + 1);
   };
 
@@ -26,7 +51,12 @@ function GameBoard() {
   return (
     <div className="gameboard" onClick={clickCallback}>
       {tableCards.map((card) => (
-        <Card key={card.id} src={card.url} />
+        <Card
+          key={card.id}
+          src={card.url}
+          id={card.id}
+          customClass={isSelectedCard(card.id) ? "selected" : ""}
+        />
       ))}
     </div>
   );
