c2330baa80b8669eec412419ff5b2071dd1a60b9
frarosset <37275581+frarosset@users.noreply.github.com>
2025-02-06 11:11:45 +0100

    feat(useDeck): add useDeck(desiredDeckSize) custom hook to handle the deck
    The useDeck(desiredDeckSize) custom hook has been defined to handle the
    deck array containing the info of the possible cards to use, and making
    it have desiredDeckSize size.
    The deck is returned by the hook, however, is does not necessarily have
    desiredDeckSize size initially, because such deck is filled
    asyncronously to deckSize by useDeck hook.
    Also, the returned deck should be considered immutabile (it is a state
    inside the hook).
    Passing a larger deckSize updates the deck with new cards. A render is
    always triggered when the deck is updated.
    
    The useDeck has been used within the GameBoard component to handle the
    deck in thre.
    A deckSize state initialized to 4 has been defined in there and used as
    desiredDeckSize input for the useDeck hook.
    A click callback on the component which increases deckSize by 1 has been
    temporarily defined to test the hook, too.
    
    The hook has been tested against some possible race conditions occurring
    in development (with StrictMode enabled) or when multiple size updates
    are issued quickly, also enabling low-end mobile device on DevTools, to
    simulate slow fetching.

--- src/components/game/GameBoard.jsx
+++ src/components/game/GameBoard.jsx
@@ -1,20 +1,30 @@
+import { useState } from "react";
 import Card from "./Card.jsx";
+import useDeck from "./useDeck.js";
 import "../../styles/GameBoard.css";
 
 function GameBoard() {
-  // The deck is temporarily just a local variable which contains some possible cards to use.
-  // and initialized with sample data to test the Card component.
-  // Later on it will be handled differently and the card faces will be grabbed through API calls.
-  const deck = [
-    { id: 0, url: "/card-rear.jpg" },
-    { id: 1, url: "/card-rear.jpg" },
-    { id: 2, url: "/card-rear.jpg" },
-  ];
+  // The imposed size of the deck, which contains the possible cards to use.
+  // It is not necessarily equal to the actual size of the deck defined next,
+  // because such deck is filled asyncronously to deckSize by useDeck() custom hook.
+  const [deckSize, setDeckSize] = useState(4);
 
+  // The deck is handled internally by the useDeck custom hook.
+  // The returned deck should not be modified.
+  // Passing a larger deckSize updates the deck with new cards: a render is always
+  // triggered when the operation is completed
+  const deck = useDeck(deckSize);
+
+  // Temporary click callback to test the deck handling of useDeck
+  const clickCallback = () => {
+    setDeckSize((x) => x + 1);
+  };
+
+  // Currently, just to test, just render the full deck
   const tableCards = deck;
 
   return (
-    <div className="gameboard">
+    <div className="gameboard" onClick={clickCallback}>
       {tableCards.map((card) => (
         <Card key={card.id} src={card.url} />
       ))}
---
+++ src/components/game/useDeck.js
@@ -0,0 +1,71 @@
+import { useState, useEffect, useRef } from "react";
+import fetchCardsData from "../../helper/fetching/fetchCardsData.js";
+
+function useDeck(requestedDeckSize) {
+  // The deck is an array containing the info of the possible cards to use, and is a
+  // state variable, so modifying it always triggers a re-render.
+  // The deckSize input specify the desired size of the deck. On first mount, this is
+  // used as initial size of the deck and deckSize cards info are fetched. A re-render
+  // is triggered whenever deckSize increases, as there are new cards to fetch.
+  //
+  // Fetching is done through a useEffect hook which is evaluated whenever deck or
+  // requestedDeckSize change.An internal ref cardsToFetch is defined and used within
+  // such effect to keep track of the cards to fetch:
+  //
+  // - if cardsToFetch.current is 0, it means there are no ongoing fetch requests,
+  //   so a new value for it is computed based on requestedDeckSize and deck.length.
+  // - then, if cardsToFetch.current is non-zero, a fetch is initialized.
+  //
+  //   If this is successful, cardsToFetch.current is set to 0 and the deck is updated.
+  //   Otherwise, cardsToFetch.current is not reset to 0. On successive re-renders, the
+  //   failed request is re-attemped using the old cardsToFetch.current: if this is
+  //   successful, the effect is run again to check whether more fetching are needed
+  //   (this avoids increasing the number of fetch attampts at each fail / abort).
+  //
+  // To avoid race conditions in development (with StrictMode enabled) or when multiple
+  // size update are issued quickly, an AbortController has been used.
+  // see: https://maxrozen.com/race-conditions-fetching-data-react-with-useeffect
+  //
+  // Note: The hook has been tested against some possible race conditions occurring in
+  // development (with StrictMode enabled) or when multiple size updates are issued quickly,
+  // also enabling low-end mobile device on DevTools, to simulate slow fetching.
+
+  const [deck, setDeck] = useState([]);
+  const cardsToFetch = useRef(requestedDeckSize);
+
+  useEffect(() => {
+    console.log("EffectInDeck", {
+      requestedDeckSize: requestedDeckSize,
+      currentDeckSize: deck.length,
+      cardsToFetch: cardsToFetch.current,
+    });
+
+    if (cardsToFetch.current == 0) {
+      cardsToFetch.current = Math.max(requestedDeckSize - deck.length, 0);
+    }
+
+    if (cardsToFetch.current > 0) {
+      const abortController = new AbortController();
+
+      const fetchData = async (n) => {
+        try {
+          const newCards = await fetchCardsData(n, abortController.signal);
+          cardsToFetch.current = 0;
+          setDeck(structuredClone([...deck, ...newCards]));
+        } catch (e) {
+          console.log(e.message);
+        }
+      };
+
+      fetchData(cardsToFetch.current);
+
+      return () => {
+        abortController.abort();
+      };
+    }
+  }, [requestedDeckSize, deck]);
+
+  return deck;
+}
+
+export default useDeck;
