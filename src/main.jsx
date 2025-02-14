8fe86d7dd61709d1054640edc943498099b77763
frarosset <37275581+frarosset@users.noreply.github.com>
2025-02-14 10:33:47 +0100

    feat(CreditFooter): add CreditFooter component

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./styles/variables.css";
 import App from "./App.jsx";
+import CreditFooter from "./components/CreditFooter.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
+    <CreditFooter />
   </StrictMode>
 );
