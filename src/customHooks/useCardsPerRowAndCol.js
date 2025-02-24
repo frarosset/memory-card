aede9ef6f56ab5a7e04a2357e30214577f6d00c0
frarosset <37275581+frarosset@users.noreply.github.com>
2025-02-24 11:49:48 +0100

    fix(useCardsPerRowAndCol): avoid using unnecessary useCallback

--- src/customHooks/useCardsPerRowAndCol.js
+++ src/customHooks/useCardsPerRowAndCol.js
@@ -1,4 +1,4 @@
-import { useRef, useEffect, useCallback } from "react";
+import { useRef, useEffect } from "react";
 
 function useCardsPerRowAndCol(
   numOfCards,
@@ -7,48 +7,66 @@ function useCardsPerRowAndCol(
 ) {
   const ref = useRef(null);
 
-  const getCardsPerRow = useCallback(() => {
-    if (!ref.current) return;
-
-    const rect = ref.current.getBoundingClientRect();
-    const width = rect.width;
-    const height = rect.height;
-
-    let maxSize = 0;
-    let maxCardsPerRow;
-    let maxCardsPerCol;
-
-    // try every possible number of columns in which the cards are distributed as a grid
-    // first, estimate the corresponding number of rows of such grid
-    // then, estimate the cards size so that they fit the container
-    // look for the number of rows/cols so that the card size is the largest
-    for (let cols = 1; cols <= numOfCards; cols++) {
-      const rows = Math.ceil(numOfCards / cols);
-
-      const size = Math.min(width / cols, height / rows);
-
-      if (size > maxSize) {
-        maxSize = size;
-        maxCardsPerRow = cols;
-        maxCardsPerCol = rows;
-      }
-    }
-
-    // set the final cards grid size as css variables
-    ref.current.style.setProperty(cardsPerRowCssVar, maxCardsPerRow);
-    ref.current.style.setProperty(cardsPerColCssVar, maxCardsPerCol);
-  }, [ref, numOfCards, cardsPerRowCssVar, cardsPerColCssVar]);
-
   useEffect(() => {
-    getCardsPerRow();
-    window.addEventListener("resize", getCardsPerRow);
+    const getCardsPerRowCallback = () => {
+      getCardsPerRow(
+        ref.current,
+        numOfCards,
+        cardsPerRowCssVar,
+        cardsPerColCssVar
+      );
+    };
+
+    getCardsPerRow(
+      ref.current,
+      numOfCards,
+      cardsPerRowCssVar,
+      cardsPerColCssVar
+    );
+    window.addEventListener("resize", getCardsPerRowCallback);
 
     return () => {
-      window.removeEventListener("resize", getCardsPerRow);
+      window.removeEventListener("resize", getCardsPerRowCallback);
     };
-  }, [getCardsPerRow]);
+  }, [ref, numOfCards, cardsPerRowCssVar, cardsPerColCssVar]);
 
   return ref;
+}
+
+function getCardsPerRow(
+  element,
+  numOfCards,
+  cardsPerRowCssVar,
+  cardsPerColCssVar
+) {
+  if (!element) return;
+  const rect = element.getBoundingClientRect();
+  const width = rect.width;
+  const height = rect.height;
+
+  let maxSize = 0;
+  let maxCardsPerRow;
+  let maxCardsPerCol;
+
+  // try every possible number of columns in which the cards are distributed as a grid
+  // first, estimate the corresponding number of rows of such grid
+  // then, estimate the cards size so that they fit the container
+  // look for the number of rows/cols so that the card size is the largest
+  for (let cols = 1; cols <= numOfCards; cols++) {
+    const rows = Math.ceil(numOfCards / cols);
+
+    const size = Math.min(width / cols, height / rows);
+
+    if (size > maxSize) {
+      maxSize = size;
+      maxCardsPerRow = cols;
+      maxCardsPerCol = rows;
+    }
+  }
+
+  // set the final cards grid size as css variables
+  element.style.setProperty(cardsPerRowCssVar, maxCardsPerRow);
+  element.style.setProperty(cardsPerColCssVar, maxCardsPerCol);
 }
 
 export default useCardsPerRowAndCol;
